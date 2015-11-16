3e4a2ef8358e64d35c32e49bfbad15dee1bfb85c
ydst22502 <turtle920@foxmail.com>
2015-11-16 21:15:42 +0800

    update

--- modules/api/controllers/TbUserinfoController.php
+++ modules/api/controllers/TbUserinfoController.php
@@ -18,17 +18,29 @@ class TbUserinfoController extends Controller
     }
 
     /********
-    *验证用户是否重名或重邮箱
+    *验证用户是否重邮箱
+    *******/
+    public function actionDuplicationOfEmail()
+    {
+      $email = Yii::$app->request->post('email');
+      $count_email=TbUserinfo::find()->andWhere(['email' => $email])->count('userid');
+
+      if ($count_email==0) {
+        return '1';//没有重名或注册过的邮箱
+      }else{
+        return '-1';//有重名或注册过的邮箱
+      }
+    }
+
+    /********
+    *验证用户是否重名
     *******/
     public function actionDuplicationOfName()
     {
       $username = Yii::$app->request->post('username');
-      $email = Yii::$app->request->post('email');
-
       $count_username=TbUserinfo::find()->andWhere(['username' => $username])->count('userid');
-      $count_email=TbUserinfo::find()->andWhere(['email' => $email])->count('userid');
 
-      if ($count_username==0&&$count_email==0) {
+      if ($count_username==0) {
         return '1';//没有重名或注册过的邮箱
       }else{
         return '-1';//有重名或注册过的邮箱
@@ -54,10 +66,11 @@ class TbUserinfoController extends Controller
         $userinfo->imagedir = $imagedir;
         $userinfo->authkey = $authkey;
         $userinfo->authsalt = $authsalt;
-        if ($userinfo->save() > 0) {
-            return '1';//添加成功
+        $userid = $userinfo->save();
+        if ($userid > 0) {
+            return BaseJson::encode(['flag'=>'1', 'userid'=>$userid, 'token'=>$authsalt]);//添加成功
         } else {
-            return '-1';//添加失败
+            return BaseJson::encode(['flag'=>'-1', 'userid'=>'-1', 'token'=>'-1']);//添加失败
         }
       }
 
